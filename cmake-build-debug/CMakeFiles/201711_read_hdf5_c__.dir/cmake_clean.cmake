a21d93b32b7ee0e6906197584dfc21c71e5342ee
Jason Cao <jc6933@nyu.edu>
2017-11-10 11:44:07 -0500

    codes

---
+++ cmake-build-debug/CMakeFiles/201711_read_hdf5_c__.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/201711_read_hdf5_c__.dir/main.cpp.o"
+  "201711_read_hdf5_c__.pdb"
+  "201711_read_hdf5_c__"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/201711_read_hdf5_c__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/201711_read_hdf5_c__.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 201711_read_hdf5_c__.
+# This may be replaced when dependencies are built.
